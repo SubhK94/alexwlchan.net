52c6d79855271e09ac86f8d599577823eca85a01
Alex Chan <alex@alexwlchan.net>
2018-07-02 07:48:40 +0100

    Start ignoring logs based on user agents

--- analytics/ignores.rb
+++ analytics/ignores.rb
@@ -1,8 +1,31 @@
+IGNORE_USER_AGENT_FRAGMENTS = [
+  "bingbot/2.0",
+]
+
+
+# Can we ignore logs from this user agent?
+#
+# For example: crawlers, spiders, automated requests.
+#
+def user_agent_should_be_ignored(user_agent)
+  if IGNORE_USER_AGENT_FRAGMENTS.any? { |frag| user_agent.include? frag }
+    return true
+  end
+
+  false
+end
+
+
 # Decides if a given path is "interesting" for the purpose of log analysis.
 #
 # Params:
 # +log+:: The parsed log line.
 #
 def should_be_ignored(parsed_log)
+
+  if user_agent_should_be_ignored(parsed_log["user_agent"])
+    return true
+  end
+
   return false
 end
